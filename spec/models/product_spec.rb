42aefb8101d7c73b6d9144b648a979b176f6e448
Coreen Huang <coreen.huang@gmail.com>
2022-10-27 13:52:52 -0400

    rspec errors full messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -17,6 +17,7 @@ RSpec.describe Product, type: :model do
         @category = Category.new(id: 3, name: "new_category")
         @product = Product.new(name: "", price_cents: 3600, quantity: 9, category: @category)
         expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Name can't be blank")
       end
     end
 
@@ -25,6 +26,7 @@ RSpec.describe Product, type: :model do
         @category = Category.new(id: 3, name: "new_category")
         @product = Product.new(name: "name", price_cents: "", quantity: 9, category: @category)
         expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Price can't be blank")
       end
     end
 
@@ -33,6 +35,7 @@ RSpec.describe Product, type: :model do
         @category = Category.new(id: 3, name: "new_category")
         @product = Product.new(name: "name", price_cents: 8900, quantity: "", category: @category)
         expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Quantity can't be blank")
       end
     end
 
@@ -41,6 +44,7 @@ RSpec.describe Product, type: :model do
         @category = Category.new(id: 3, name: "new_category")
         @product = Product.new(name: "name", price_cents: 8900, quantity: 10, category: nil)
         expect(@product).to_not be_valid
+        expect(@product.errors.full_messages).to include("Category can't be blank")
       end
     end
   end
